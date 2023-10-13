3d06d0e7b2df966d6163d05bf3819f0caa4a3c87
Admin <diegovillalobos503@gmail.com>
2023-10-13 12:23:38 -0600

    2 parte

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,13 +1,10 @@
-import 'dart:js';
-
+/*
 import 'package:go_router/go_router.dart';
-
 import '../../presentation/screens/screens.dart';
 //aca definimos el tipo, de moneda
 //lo que tratamos en esta clase es evitar de poner varios ceros el cual va retornar moneda,simbolo,moneda las cuales las definimos en esta clase
 //este formato lo podemos utilizar para cuando vamos a usar o tranferir dinero
 //formato de numero ya definido,
-
 
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
@@ -23,3 +20,4 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
     ]
   ),
 ]);
+*/
